724dac426ba346693205c1fd7da0d9f03eb683eb
Vladimir_Rydvan <rvltlt@yandex.ru>
2019-01-14 00:15:38 +0400

    converts svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import testRender02 from './testRender02.svg';
+import originalSvg from './originalSvg.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +8,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <p>This original svg</p>
+          <img src={originalSvg} className="App-logo" alt="logo" />
+          <p>This converted svg</p>
+          <img src={testRender02} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
